7d7bf2c258c4948a9890c66842040d11fcc11200
starmatch9 <18534538017@163.com>
2025-09-17 14:21:07 +0800

    塔等级限制

---
+++ Assets/Scripts/IndieClasses/LevelLock.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLock : MonoBehaviour
+{
+    public int nextLevel = 0;
+
+    public GameObject Lock = null;
+    public GameObject Lock2 = null;
+
+    private void Start()
+    {
+        if(Lock != null)
+        {
+            if(nextLevel > GlobalLimit.level)
+            {
+                Lock.SetActive(true);
+            }
+            else
+            {
+                Lock.SetActive(false);
+            }
+        }
+
+        if (Lock2 != null) 
+        {
+            if (nextLevel > GlobalLimit.level)
+            {
+                Lock2.SetActive(true);
+            }
+            else
+            {
+                Lock2.SetActive(false);
+            }
+        }
+    }
+
+}
